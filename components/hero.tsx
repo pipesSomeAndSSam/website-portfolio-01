051a7c3f45d8b538244ab8103206a60a1a1a2c60
pipes <144916002+pipesSomeAndSSam@users.noreply.github.com>
2026-05-10 00:01:19 +0800

    minor fixes

--- components/hero.tsx
+++ components/hero.tsx
@@ -53,8 +53,7 @@ export default function Hero() {
             </h2>
             <p className="text-lg text-muted-foreground mb-8 max-w-lg">
               I am a Computer Science student in Visayas States University and I
-              dream to become a UI/UX Designer. That's creativity in determining
-              the design that fits the company's goals.
+              dream to become a UI/UX Designer.
             </p>
 
             <div className="flex flex-col sm:flex-row gap-4">
